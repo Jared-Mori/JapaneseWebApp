b4c7c02ffb004f77c7450768ba50c67826a0d324
jared <jared@Mori.us>
2025-06-10 17:18:38 -0700

    working version 1

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <script>
-    let { data } = $props();
-  </script>
+  let { data } = $props();
+
+  // For debugging: log the data to the console
+  console.log("Loaded data:", data);
+</script>
+
+{#if data && data.instruments && data.instruments.length > 0}
   <ul>
     {#each data.instruments as instrument}
       <li>{instrument.name}</li>
     {/each}
   </ul>
+{:else}
+  <p>No instruments found or data not loaded.</p>
+{/if}
+
+<pre>{JSON.stringify(data, null, 2)}</pre>
